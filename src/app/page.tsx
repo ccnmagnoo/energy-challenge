214e6ce755c1d6ea4832cab4e87502d264c15139
ccnmagnoo <crcamposn@gmail.com>
2023-11-16 15:24:03 -0300

    background gradient

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import styles from './page.module.css';
+import logo from '@/app/static/eg-logo-loop.svg';
+import Image from 'next/image';
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <section>main section</section>
+      <section>
+        <Image src={logo} width={'1000'} alt={'logo'} />
+      </section>
     </main>
   );
 }
