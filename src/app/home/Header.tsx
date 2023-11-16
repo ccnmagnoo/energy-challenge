87b2b18a8ad6ecd80dfc9fb38de33e5919eb6e17
ccnmagnoo <crcamposn@gmail.com>
2023-11-16 14:17:51 -0300

    base layout

---
+++ src/app/home/Header.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Image from 'next/image';
+import eg_logo from '@/app/static/eg-logo-short.svg';
+import styles from './Home.module.scss';
+
+export const Header = () => {
+  return (
+    <header>
+      <section className={styles.logo}>
+        <Image src={eg_logo} height={100} width={100} alt='eg' />
+      </section>
+      <nav>
+        <ul>
+          <li>link</li>
+          <li>link</li>
+          <li>link</li>
+        </ul>
+      </nav>
+      <section className={styles.session}>session</section>
+    </header>
+  );
+};
