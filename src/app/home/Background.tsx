969b5a9a1f7ac5ce4d20e21e34c099381300a71b
ccnmagnoo <crcamposn@gmail.com>
2023-11-20 11:54:02 -0300

    drei for Html content inside fiber canvas

--- src/app/home/Background.tsx
+++ src/app/home/Background.tsx
@@ -1,12 +1,12 @@
 import React, { Suspense } from 'react';
 import { Canvas } from '@react-three/fiber';
 import styles from './Background.module.scss';
-
+import { Html } from '@react-three/drei';
 export const Background = () => {
   return (
     <section>
       <Canvas className={styles.canvas} camera={{ near: 0.1, far: 1000 }}>
-        <Suspense fallback={'loading 3D model...'}></Suspense>
+        <Suspense fallback={<Html>loading 3D model...</Html>}></Suspense>
       </Canvas>
     </section>
   );
