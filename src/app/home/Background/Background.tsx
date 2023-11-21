16c23b5fe5b4d868751ae699314ad98dd913aa8c
ccnmagnoo <crcamposn@gmail.com>
2023-11-21 14:38:46 -0300

    try loading scene with glbt tool

--- src/app/home/Background/Background.tsx
+++ src/app/home/Background/Background.tsx
@@ -3,11 +3,14 @@ import React, { Suspense } from 'react';
 import { Canvas } from '@react-three/fiber';
 import styles from './Background.module.scss';
 import { Html } from '@react-three/drei';
+import { Island } from './Island';
 export const Background = () => {
   return (
     <section className={styles.background_container}>
       <Canvas className={styles.canvas} camera={{ near: 0.1, far: 1000 }}>
-        <Suspense fallback={<Html>loading 3D model...</Html>}></Suspense>
+        <Suspense fallback={<Html>loading 3D model...</Html>}>
+          <Island></Island>
+        </Suspense>
       </Canvas>
     </section>
   );
